4e2bd1015e72bd6595596ae5521330c5be133b0b
bitcoineazy <bitcoineazy@gmail.com>
2021-09-20 03:26:14 +0300

    static

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -146,10 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = 'static/'
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
